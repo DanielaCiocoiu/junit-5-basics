9038d5b6ac6741f921bd64fdff12ad27ff99f2bb
shantiox <danaciocoiu78@gmail.com>
2019-06-13 19:07:12 +0300

    assertThrows

--- src/main/java/io/home/MathUtils.java
+++ src/main/java/io/home/MathUtils.java
@@ -2,15 +2,23 @@ package io.home;
 
 public class MathUtils {
 	
-public int add(int a, int b) {
-	return a+b;
+	public int add(int a, int b) {
+		return a + b;
+	}
 	
-}
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a/b;
+	}
 	
 	public double computeCircleArea(double radius) {
-		
-		return Math.PI  * radius * radius;
-		
-		
+		return Math.PI * radius * radius;
 	}
 }
--- src/test/java/io/home/MathUtilsTest.java
+++ src/test/java/io/home/MathUtilsTest.java
@@ -6,20 +6,53 @@ import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
-	@Test
-	void testAdd() {
-		MathUtils mathUtils  = new MathUtils();
-		int expected = 2;
-		int actual = mathUtils.add(1, 1);
-		mathUtils.add(1, 1);
-		assertEquals(expected, actual);
+private MathUtils mathUtils;
+	
+	
+	@BeforeEach
+	void initEach() {
+		mathUtils = new MathUtils();
 	}
 
+	@Nested
+	class AddTest {
+		@Test
+		void testAddingTwoPositives() {
+			assertEquals(2, mathUtils.add(1, 1), 
+					"Add method should return the sum of two numbers");
+		}
+		
+		@Test
+		void testAddingTwoNegatives() {
+			assertEquals(-2, mathUtils.add(-1, -1), 
+					"Add method should return the sum of two numbers");
+		}
+		
+		@Test
+		void testAddingAPositiveAndANegative() {
+			assertEquals(0, mathUtils.add(-1, 1), 
+					"Add method should return the sum of two numbers");
+		}
+	}
+	
+	@Test 
+	void testMultiply() {
+		assertAll(
+				() -> assertEquals(0, mathUtils.multiply(1, 0)),
+				() -> assertEquals(1, mathUtils.multiply(1, 1)),
+				() -> assertEquals(6, mathUtils.multiply(2, 3))
+				);
+	}
+	
+	@Test 
+	void computeCircleArea() {
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), 
+				"Should return right circle area");
+	}
 	
 	@Test
-	void testComputeCircleArea() {
-		MathUtils mathUtils  = new MathUtils();
-		
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
+				"Divide should throw ArithmeticException when denominator is zero");
 	}
 }
